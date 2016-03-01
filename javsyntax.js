e9e0eebdbffcec76d91dd15c7a2c843c435e3a59
Santiago Garza <sgarzaceja@me.com>
2016-02-29 20:06:05 -0500

    completed exercises

---
+++ javsyntax.js
@@ -0,0 +1,82 @@
+// RUBY
+// my_name = "Emmett Brown"
+// puts my_name
+//
+// JS
+
+//
+// console.log("I am ...")
+// console.log(my_name)
+
+//
+// var my_name = "emmett brown"
+// // console.log(my_name)
+// console.log("I am..."+my_name.toUpperCase())
+
+//
+//
+// function capitalizeEachWord(str) {
+//     return str.replace(/\w\S*/g, function(txt) {
+//         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     });
+// }
+//
+// var x = my_name;
+// var y = capitalizeEachWord(x);
+//
+// console.log("I am...")
+// console.log(y);
+
+// var journey = "We're going to October 21, 2015.. the FUTURE!!"
+// var substring = journey.replace("FUTURE", "PAST");
+// console.log(substring);
+
+// var a = "The quick brown fox jumped over the lazy dog.";
+//
+// var pattern = /the/ig;
+//
+// a.replace( pattern, "THE" );
+// THE quick brown fox jumped over THE lazy dog.
+
+// number_of_hoverboards = "1"
+// var string = "There are at least number_of_hoverboards hoverboard(s) in 2015";
+// var string_to_integer = parseInt(number_of_hoverboards)
+// console.log(string.replace("number_of_hoverboards", string_to_integer))
+
+// var hoverboards = 10;
+//
+// if (hoverboards >= 100){
+//   console.log('The future rocks!');
+// } else if (hoverboards < 100 && hoverboards > 1){
+//   console.log('The future is pretty cool');
+// } else if (hoverboards === 1){
+//   console.log('Meh');
+// } else {
+//   console.log('The future is boring');
+// }
+
+// console.log("Let's Go!")
+//
+// var i = 0;
+//
+// while(i <= 89){
+//   if(i === 88){
+//   console.log("ZOOOOOM");
+// }else{
+// console.log(i);
+// }
+// i++;
+// }
+//
+// var actors = ["Michael Fox", "Christopher Lloyd", "Lea Thompson", "Claudia Wells"];
+//
+// for (var i=0; i < actors.length; i++) {
+//   console.log(actors[i] + " is so cool.");
+// }
+
+// function greeting(name){
+//   console.log("Hello, " + name + ".");
+// }
+//
+// greeting("Pedro");
+//
